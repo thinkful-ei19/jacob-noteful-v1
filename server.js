e1c41377e5503c618b8ec1b4bf75203f160076b4
JacobAZimmerman <ijacobzim@gmail.com>
2018-03-05 16:38:18 -0800

    added api get methods, along with search feature

--- server.js
+++ server.js
@@ -8,3 +8,23 @@ const notes = simDB.initialize(data);
 console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  searchTerm ? 
+    res.json(data.filter(item => item.title.includes(searchTerm))) : 
+    res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  res.json(data.find(item => item.id === Number(req.params.id)));
+});
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
